afe232755b05744aa53029e1be77ec4270b2b610
Joel Hernandez <jhernandez98@outlook.es>
2020-09-21 09:03:37 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const searchIcon = document.querySelector("#search");
+const hamburgerIcon = document.querySelector(".hamburger-container");
+const menuBar = document.querySelector(".menu-bar");
+const body = document.querySelector("body");
+const searchBox = document.querySelector(".search-box");
+
+
 
 const mobiiltyContainer = document.querySelector(".mobility");
 
@@ -15,9 +21,85 @@ const playVideo = () => {
 };
 
 const showSearchBox = () => {
-	const searchBox = document.querySelector(".search-box");
+	
+	const input = searchBox.querySelector("input");
 	searchBox.classList.toggle("showSearchBox");
+	input.focus();
+
+	if(menuBar.classList.contains("open"))
+	{
+		menuBar.classList.toggle("open");
+		movetoLeft();
+	}
+
 };
+
+const openMenuBar = () =>
+{
+	movetoLeft();
+	//setTimeout(activateOverlay,50);
+	menuBar.classList.toggle("open");
+
+	if(searchBox.classList.contains("showSearchBox"))
+	{
+		searchBox.classList.toggle("showSearchBox");
+	}
+}
+
+function movetoLeft()
+{
+	const bannerSection = document.querySelector(".banner");
+	const airbagSection = document.querySelector(".airbag-alert");
+	const gallerySection = document.querySelector(".gallery");
+
+	const arraySection = [bannerSection,airbagSection,gallerySection];
+
+	arraySection.forEach(section=>{
+		section.classList.toggle("move");
+	})
+	
+	hideScrollBar();
+}
+/* function activateOverlay()
+{
+	const overlayCover = document.querySelector(".overlay-cover");
+	overlayCover.classList.toggle("active");
+} */
+
+function hideScrollBar()
+{
+	body.classList.toggle("hideScrollBar");
+}
+
+const openAccordion = (e) =>
+{
+	const isAccordionHeader = e.target.classList.contains("accordion-header");
+	let currentActiveAccordionBody = document.querySelector(".activeAccordion");
+	const accordionBodyActive = e.target.nextElementSibling;
+
+	
+
+	if(isAccordionHeader)
+	{
+		
+		if(currentActiveAccordionBody===null)
+		{
+			currentActiveAccordionBody= accordionBodyActive;
+			currentActiveAccordionBody.classList.add("activeAccordion");
+			return;
+		}
+		
+		if(currentActiveAccordionBody ===accordionBodyActive)
+		{
+		   currentActiveAccordionBody.classList.remove("activeAccordion");
+		   return;
+		}
+
+		 currentActiveAccordionBody.classList.remove("activeAccordion");
+		accordionBodyActive.classList.add("activeAccordion"); 
+	
+	}
+}
 
 function spinSearchIcon() {}
 
@@ -25,3 +107,5 @@ function spinSearchIcon() {}
 
 //mobiiltyContainer.addEventListener("mouseover", playVideo);
 searchIcon.addEventListener("click", showSearchBox);
+hamburgerIcon.addEventListener("click",openMenuBar);
+menuBar.addEventListener("click",openAccordion);
